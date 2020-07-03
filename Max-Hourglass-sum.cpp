23acc5396fb4e113ef340cd2e612d9e7fe8cca9a
Gopika Murali <gopikamurali.mec@gmail.com>
2020-07-03 17:42:54 +0530

    Changed variable names to meaningful ones

--- Max-Hourglass-sum.cpp
+++ Max-Hourglass-sum.cpp
@@ -1,13 +1,13 @@
 int hourglassSum(vector<vector<int>> arr) {
     int hour_glass_sum=0;
-    int result=INT_MIN;
+    int max_sum=INT_MIN;
     for(int i=0; i<=3; i++){
         for(int j=0; j<=3; j++){
             hour_glass_sum=arr[i][j]+arr[i][j+1]+arr[i][j+2]
                                     +arr[i+1][j+1]
                            +arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
-            result=max(hour_glass_sum,result);
+            max_sum=max(hour_glass_sum,max_sum);
         }
     }
-    return result;
+    return max_sum;
 }
